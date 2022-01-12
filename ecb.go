5643d743dc1cd88fc56bb10122385d45e5b3315c
René Post <git@xpt.nl>
2022-01-12 01:45:22 +0100

    Add Rates function to return all exchange rates

--- ecb.go
+++ ecb.go
@@ -22,59 +22,41 @@ import (
 // https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml
 // https://www.ecb.europa.eu/stats/eurofxref/eurofxref-sdmx.xml
 
-type Error string
-
-func (e Error) Error() string { return string(e) }
-
-type Daily struct {
-	Rates []Rate `xml:"Cube>Cube"`
-}
-
-type Rate struct {
-	Time       string     `xml:"time,attr"`
-	Currencies []Currency `xml:"Cube"`
-}
-
 type Symbol string
-
-type Currency struct {
-	Symbol Symbol `xml:"currency,attr"`
-	Rate   string `xml:"rate,attr"`
-}
 
 const (
 	USD Symbol = "USD"
-	JPY        = "JPY"
-	BGN        = "BGN"
-	CZK        = "CZK"
-	DKK        = "DKK"
-	GBP        = "GBP"
-	HUF        = "HUF"
-	PLN        = "PLN"
-	RON        = "RON"
-	SEK        = "SEK"
-	CHF        = "CHF"
-	ISK        = "ISK"
-	NOK        = "NOK"
-	HRK        = "HRK"
-	RUB        = "RUB"
-	TRY        = "TRY"
-	AUD        = "AUD"
-	BRL        = "BRL"
-	CAD        = "CAD"
-	CNY        = "CNY"
-	HKD        = "HKD"
-	IDR        = "IDR"
-	ILS        = "ILS"
-	INR        = "INR"
-	KRW        = "KRW"
-	MXN        = "MXN"
-	MYR        = "MYR"
-	NZD        = "NZD"
-	PHP        = "PHP"
-	SGD        = "SGD"
-	THB        = "THB"
-	ZAR        = "ZAR"
+	JPY Symbol = "JPY"
+	BGN Symbol = "BGN"
+	CZK Symbol = "CZK"
+	DKK Symbol = "DKK"
+	GBP Symbol = "GBP"
+	HUF Symbol = "HUF"
+	PLN Symbol = "PLN"
+	RON Symbol = "RON"
+	SEK Symbol = "SEK"
+	CHF Symbol = "CHF"
+	ISK Symbol = "ISK"
+	NOK Symbol = "NOK"
+	HRK Symbol = "HRK"
+	RUB Symbol = "RUB"
+	TRY Symbol = "TRY"
+	AUD Symbol = "AUD"
+	BRL Symbol = "BRL"
+	CAD Symbol = "CAD"
+	CNY Symbol = "CNY"
+	HKD Symbol = "HKD"
+	IDR Symbol = "IDR"
+	ILS Symbol = "ILS"
+	INR Symbol = "INR"
+	KRW Symbol = "KRW"
+	MXN Symbol = "MXN"
+	MYR Symbol = "MYR"
+	NZD Symbol = "NZD"
+	PHP Symbol = "PHP"
+	SGD Symbol = "SGD"
+	THB Symbol = "THB"
+	ZAR Symbol = "ZAR"
 )
 
 // EUR returns the rate of the Euro denominated in the currency passed in as an argument.
@@ -84,7 +66,7 @@ func EUR(symbol Symbol) (rate float64, err error) {
 		return 0.0, err
 	}
 	defer response.Body.Close()
-	daily := Daily{}
+	daily := daily{}
 	if err := xml.NewDecoder(response.Body).Decode(&daily); err != nil {
 		return 0.0, err
 	}
@@ -99,3 +81,45 @@ func EUR(symbol Symbol) (rate float64, err error) {
 	}
 	return 0.0, Error(fmt.Sprint("symbol", symbol, "not found"))
 }
+
+// Rates returns all the rates of the Euro denominated in other currencies.
+func Rates() (rates map[Symbol]float64, err error) {
+	response, err := http.Get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	daily := daily{}
+	if err := xml.NewDecoder(response.Body).Decode(&daily); err != nil {
+		return nil, err
+	}
+
+	rates = make(map[Symbol]float64)
+	for _, currency := range daily.Rates[0].Currencies {
+		cr, err := decimal.NewFromString(currency.Rate)
+		if err != nil {
+			return nil, err
+		}
+		rate, _ := cr.Float64()
+		rates[currency.Symbol] = rate
+	}
+	return rates, nil
+}
+
+type daily struct {
+	Rates []rate `xml:"Cube>Cube"`
+}
+
+type rate struct {
+	Time       string     `xml:"time,attr"`
+	Currencies []currency `xml:"Cube"`
+}
+
+type currency struct {
+	Symbol Symbol `xml:"currency,attr"`
+	Rate   string `xml:"rate,attr"`
+}
+
+type Error string
+
+func (e Error) Error() string { return string(e) }
